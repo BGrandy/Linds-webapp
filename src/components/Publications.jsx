2c29570b23109df370e6a06f0d91ae91152c36b4
BGrandy <bengrandy98@gmail.com>
2025-01-10 18:32:21 -0500

    Publication and Team page
    -Added
    grid layout for Publication and Team page
    Filler info
    Footer
    
    -Required
    More formatting to Publication page
    Formated Footer

--- src/components/Publications.jsx
+++ src/components/Publications.jsx
@@ -1,6 +1,44 @@
-function Publications(){
-    return(
-        <></>
+import '../styles/Publications.css'
+import img1 from '../assets/img1.gif'
+import img2 from '../assets/img2.jpg'
+import img3 from '../assets/img3.jpg'
+import img4 from '../assets/img4.jpg'
+import img5 from '../assets/Untitled.png'
+
+function Publications() {
+    return (
+        <>
+            <div className='publication-title'><h2>Selected Publications</h2></div>
+            <div className='grid-container'>
+
+                <div>
+                    <img src={img1}></img>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sed augue  elementum, porttitor odio nec, accumsan magna. Sed vel mi vitae odio  suscipit rhoncus in nec nunc.</p>
+                </div>
+                <div>
+                    <img src={img2}></img>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sed augue  elementum, porttitor odio nec, accumsan magna. Sed vel mi vitae odio  suscipit rhoncus in nec nunc.</p>
+                </div>
+                <div>
+                    <img src={img3}></img>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sed augue  elementum, porttitor odio nec, accumsan magna. Sed vel mi vitae odio  suscipit rhoncus in nec nunc.</p>
+                </div>
+                <div>
+                    <img src={img4}></img>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sed augue  elementum, porttitor odio nec, accumsan magna. Sed vel mi vitae odio  suscipit rhoncus in nec nunc.</p>
+                </div>
+                <div>
+                    <img src={img5}></img>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sed augue  elementum, porttitor odio nec, accumsan magna. Sed vel mi vitae odio  suscipit rhoncus in nec nunc.</p>
+                </div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+
+        </>
     )
 }
 
